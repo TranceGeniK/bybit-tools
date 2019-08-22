cd74aa08ff3a8ce0cd41e7d9b692c3e631bc1627
Pierrick Letos <pierrick.letos@gmail.com>
2019-08-22 18:48:17 +0200

    add entry price to open position

--- src/components/Home/OpenPosition/OpenPosition.js
+++ src/components/Home/OpenPosition/OpenPosition.js
@@ -8,16 +8,14 @@ export default {
         {text: 'Open Position', value: 'symbol'},
         {text: 'Qty', value: 'size'},
         {text: 'Value', value: 'position_value'},
+        {text: 'Price', value: 'entry_price'},
         {text: 'Liq. Price', value: 'liq_price'},
         {text: 'Margin', value: 'position_margin'},
         {text: 'Leverage', value: 'leverage'},
         {text: 'Unrealized P&L', value: 'unrealised_pnl'},
         {text: 'Stop Loss', value: 'stop_loss'},
         {text: 'Take Profit', value: 'take_profit'},
-        {text: 'Trailing Stop', value: 'trailing_stop'},
-        // {text: 'Time In Force', value: 'time_in_force'},
-        // {text: 'Updated At', value: 'updated_at'},
-        // {text: 'Cancel', value: 'cancel'},
+        {text: 'Trailing Stop', value: 'trailing_stop'}
       ]
     }
   },
