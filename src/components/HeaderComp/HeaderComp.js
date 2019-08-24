b967a2f0a9c58e9a74ac4c0985922d664fc9231d
Pierrick Letos <pierrick.letos@gmail.com>
2019-08-24 17:01:24 +0200

    color header price

--- src/components/HeaderComp/HeaderComp.js
+++ src/components/HeaderComp/HeaderComp.js
@@ -2,18 +2,26 @@ export default {
   name: 'header-comp',
   components: {},
   props: [],
-  data () {
+  data() {
     return {
-
-    }
+      increasing: false,
+      decreasing: false,
+    };
   },
-  computed: {
-
+  computed: {},
+  mounted() {
+  
   },
-  mounted () {
-
+  methods: {},
+  watch: {
+    '$bybitApi.lastPrice': function(newPrice, oldPrice) {
+      if (newPrice > oldPrice) {
+        this.increasing = true;
+        this.decreasing = false;
+      } else {
+        this.increasing = false;
+        this.decreasing = true;
+      }
+    },
   },
-  methods: {
-
-  }
-}
+};
